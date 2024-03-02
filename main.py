5277d64ba4ef4e55943a746c89db6d791a2acab5
abllu <wanzhoumosong315@gmail.com>
2024-03-03 04:28:37 +0900

    update 0303

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from flask import Flask, request, render_template, send_from_directory, url_for
-import os
+from flask import Flask, request, render_template, send_from_directory, url_for, redirect, session, render_template_string
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+import shutil
+from datetime import datetime, timedelta
 from werkzeug.utils import secure_filename
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
 import numpy as np
+import json
 
-import torch, detectron2
 # Some basic setup:
 # Setup detectron2 logger
-import detectron2
+import torch, detectron2
 from detectron2.utils.logger import setup_logger
 setup_logger()
 
@@ -23,15 +25,113 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
 
+# FlaskとFlask-Loginの初期化
 app = Flask(__name__)
-UPLOAD_FOLDER = 'uploads'
+app.secret_key = "jfwei938n329fn"
+login_manager = LoginManager()
+login_manager.init_app(app)
+UPLOAD_FOLDER = 'static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
-def upload_form():
-    return render_template('upload.html')
+# 仮のユーザー
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
 
+# ユーザーローダーの設定
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+# ログインページ
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    session['points'] = {}
+    if request.method == 'POST':
+        # ログイン処理（ここでは仮の処理を行います）
+        user_id = request.form.get('user_id')
+        user = User(user_id)
+        login_user(user)
+        return redirect(url_for('home'))
+    return render_template('login.html')
+
+# ログアウト
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    # アップロードディレクトリ内のファイルを全て削除
+    for filename in os.listdir(UPLOAD_FOLDER):
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+    return redirect(url_for('login'))
+
+# ホームページ
+@app.route('/home')
+@login_required
+def home():
+    now = datetime.now()
+    date_string = now.strftime("%Y年%m月%d日")
+    today = now.strftime("%Y-%m-%d")
+    yesterday = (now - timedelta(days=1)).strftime("%Y-%m-%d")
+    day_before_yesterday = (now - timedelta(days=2)).strftime("%Y-%m-%d")
+    display = {} #ホーム画面表示用データ格納
+    for day in [today, yesterday, day_before_yesterday]:
+        display[day] = {}
+        for time_of_day in ['morning', 'afternoon', 'evening']:
+            time_label = day + "_" + time_of_day
+            if time_label in session['points'].keys():
+                display[day][time_of_day] = session['points'][time_label]
+                display[day][time_of_day]['json'] = json.dumps(session['points'][time_label])
+            else:
+                display[day][time_of_day] = None
+    return render_template('home.html', points = session['points'], date=date_string, display=display)
+
+#画像アップロードページ
+@app.route('/upload_folder')
+@login_required
+def upload_form():
+    day = request.args.get('day', default=None, type=str)
+    time = request.args.get('time', default=None, type=str)
+    print(day, time)
+
+    now = datetime.now()
+    if day is None:
+        day = now.strftime("%Y-%m-%d")
+    if time is None:
+        hour = now.hour
+        if 4 < hour < 11: time = "morning"
+        elif 10 < hour < 17: time = "afternoon"
+        else: time = "evening"
+
+    return render_template('upload.html', day=day, time=time)
+
+#記録表示ページ
+@app.route('/record', methods=['GET'])
+@login_required
+def show_record():
+    data_str = request.args.get('data', default=None, type=str)
+    if data_str is None:
+        return "データがありません"
+    try:
+        data_dict = json.loads(data_str)
+    except json.JSONDecodeError:
+        return "JSON形式のデータの解析に失敗しました"
+    return render_template('image_display.html', data=data_dict)
+
+#結果表示ページ(画像投稿時)
 @app.route('/upload', methods=['POST'])
+@login_required
 def upload_image():
     if 'image' not in request.files:
         return "No file part"
@@ -39,15 +139,26 @@ def upload_image():
     if file.filename == '':
         return "No image selected for uploading"
     if file:
+        date = request.form['date']
+        time_of_day = request.form['time_of_day']
+        time_label = date + "_" + time_of_day
         _, file_extension = os.path.splitext(file.filename)
-        filename = f"{uuid.uuid4()}{file_extension}"
+        filename = time_label + "_" + f"{uuid.uuid4()}{file_extension}"
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(os.path.join('static', filepath))
+        file.save(filepath)
         
         # ここで画像加工処理を呼び出す
-        [processed_image_url, plaque_pro, inflammation] = process_image(os.path.join('static', filepath))
-        
-        return render_template('image_display.html', image_url=processed_image_url, plaque_pro=plaque_pro, inflammation=inflammation)
+        [processed_image_url, plaque_pro, predicted_class] = process_image(filepath)
+        class_labels = {0: '炎症あり', 1: '炎症なし'}
+        inflammation_label = class_labels[predicted_class]
+        tooth_point = int(50 * predicted_class + 50 - plaque_pro/2)
+        print(type(plaque_pro), type(predicted_class), type(tooth_point))  # データ型を確認
+        session['points'][time_label] = {'plaque': plaque_pro, 'inflammation': predicted_class, 'point': tooth_point, 'url': processed_image_url}
+        session.modified = True
+
+        print(session['points'][time_label])
+
+        return render_template('image_display.html', data=session['points'][time_label])
     else:
         return "Something went wrong"
 
@@ -96,17 +207,23 @@ def process_image(image_path):
 
     #面積を計算
     area_per_class = {}
+    masks, classes = [], [] 
     if instances.has("pred_masks"):
         masks = instances.pred_masks.numpy()  # セグメンテーションマスクをnumpy配列に変換
         classes = instances.pred_classes.numpy()  # 予測されたクラスIDをnumpy配列に変換
-    for mask, cls in zip(masks, classes):
-        # クラスIDをキーとして面積を合計
-        area = mask.sum()  # マスク内のTrueの数が面積に相当
-        if cls in area_per_class:
-            area_per_class[cls] += area
-        else:
-            area_per_class[cls] = area
-    plaque_pro = np.round(area_per_class[1] / area_per_class[2] * 100)
+    if 2 not in classes:
+        plaque_pro = int(0) #将来的には、「歯が検出できませんでした」というエラーメッセージを書く
+    elif 1 not in classes:
+        plaque_pro = int(0)
+    else:
+        for mask, cls in zip(masks, classes):
+            # クラスIDをキーとして面積を合計
+            area = mask.sum()  # マスク内のTrueの数が面積に相当
+            if cls in area_per_class:
+                area_per_class[cls] += area
+            else:
+                area_per_class[cls] = area
+    plaque_pro = int(np.round(area_per_class[1] / area_per_class[2] * 100))
     
     #炎症かどうか判定
     img_i = image.load_img(image_path, target_size=(640, 640))
@@ -114,11 +231,9 @@ def process_image(image_path):
     img_array = np.expand_dims(img_array, axis=0)
     img_array = preprocess_input(img_array)
     predictions = loaded_model.predict(img_array)
-    predicted_class = np.argmax(predictions)
-    class_labels = {0: '炎症あり', 1: '炎症なし'}
-    predicted_label = class_labels[predicted_class]
+    predicted_class = int(np.argmax(predictions)) #0: '炎症あり', 1: '炎症なし'
 
-    return [masked_im_path, plaque_pro, predicted_label]
+    return [masked_im_path, plaque_pro, predicted_class]
 
 if __name__ == "__main__":
     app.run(debug=True)
